03ef8e4218ff3f6b7afb500458d6ee56ed031114
anshumancodes <anshumanprof01@gmail.com>
2026-08-27 03:30:26 +0530

    feat(ai): export extract and map functions

---
+++ packages/ai/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./schemas/schemas";
+import { extractAnswers } from "./extract-answers";
+import { extractQuestions } from "./extract-questions";
+import { mapAnswers } from "./map-answers";
+
+
+export {extractAnswers,extractQuestions,mapAnswers}
